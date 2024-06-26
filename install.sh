61f2eafb916c4be1e09fa25338c5875b5263287b
devubu <159164402+devubu@users.noreply.github.com>
2024-06-26 19:17:22 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# Update .zshrc with alias
+alias_lines=(
+  "alias opacity='~/Tools/Custom/Bash/config_editor/alacritty/opacity.sh'"
+)
+
+# To ensure the last line of ~/.zshrc is empty
+ensure_last_line_empty() {
+  if [ -n "$(tail -n 1 ~/.zshrc)" ]; then
+    echo "" >> ~/.zshrc
+  fi
+}
+
+ensure_last_line_empty
+
+# Iterate over each alias line and check if it exists in ~/.zshrc
+for alias_line in "${alias_lines[@]}"; do
+  if ! grep -q "^$alias_line" ~/.zshrc; then
+    # If not, append the alias line to ~/.zshrc
+    echo "$alias_line" >> ~/.zshrc
+    # echo "Alias added to ~/.zshrc: $alias_line"
+  # else
+    # echo "Alias already exists in ~/.zshrc: $alias_line"
+  fi
+done
